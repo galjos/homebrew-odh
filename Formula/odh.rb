50e2883043f461bdc4b19f9304fc9bfcc9ef303d
Josef M. Gallmetzer <64498081+galjos@users.noreply.github.com>
2026-05-26 16:04:58 +0200

    Update odh formula to v0.2.2

--- Formula/odh.rb
+++ Formula/odh.rb
@@ -1,8 +1,8 @@
 class Odh < Formula
   desc "Agent-friendly CLI for public Open Data Hub APIs"
   homepage "https://github.com/galjos/odh-cli"
-  url "https://github.com/galjos/odh-cli/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "cef29af50597d6a12d634da465a49bdfe297dd17968bb763b482013b63642663"
+  url "https://github.com/galjos/odh-cli/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "cc502871ddc68f76f3c3468632ad0a7c779a0cef0ed0f736453adae487264ed6"
   license "MPL-2.0"
   head "https://github.com/galjos/odh-cli.git", branch: "main"
 
@@ -11,16 +11,16 @@ class Odh < Formula
   def install
     ldflags = %w[
       -s -w
-      -X github.com/galjos/odh-cli/internal/version.Version=0.2.1
-      -X github.com/galjos/odh-cli/internal/version.Commit=60d32b874d31
-      -X github.com/galjos/odh-cli/internal/version.Date=2026-05-26T13:33:22Z
+      -X github.com/galjos/odh-cli/internal/version.Version=0.2.2
+      -X github.com/galjos/odh-cli/internal/version.Commit=a32e3560e8d0
+      -X github.com/galjos/odh-cli/internal/version.Date=2026-05-26T14:02:37Z
     ]
 
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/odh"
   end
 
   test do
-    assert_match "odh 0.2.1", shell_output("#{bin}/odh version --format text")
+    assert_match "odh 0.2.2", shell_output("#{bin}/odh version --format text")
     system bin/"odh", "doctor", "--network=false"
   end
 end
