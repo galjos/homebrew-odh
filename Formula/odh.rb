ddf28fd2bdbce8cd02833679f063c13b05115d2f
Josef M. Gallmetzer <64498081+galjos@users.noreply.github.com>
2026-08-04 10:21:37 +0200

    Update odh formula to v0.6.0

--- Formula/odh.rb
+++ Formula/odh.rb
@@ -1,8 +1,8 @@
 class Odh < Formula
   desc "Agent-friendly CLI for public Open Data Hub APIs"
   homepage "https://github.com/galjos/odh-cli"
-  url "https://github.com/galjos/odh-cli/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "2a279c1e9b02a301c73204198b6aabf96ff5ce8964c32dda21684ecde387f2a0"
+  url "https://github.com/galjos/odh-cli/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "2845cb99b9216c3fe6eeb675b8f4479b747cf2349220b57b9ec87856d79f726b"
   license "MPL-2.0"
   head "https://github.com/galjos/odh-cli.git", branch: "main"
 
@@ -12,8 +12,8 @@ class Odh < Formula
     ldflags = %W[
       -s -w
       -X github.com/galjos/odh-cli/internal/version.Version=#{version}
-      -X github.com/galjos/odh-cli/internal/version.Commit=6c95baec169c
-      -X github.com/galjos/odh-cli/internal/version.Date=2026-08-04T07:14:17Z
+      -X github.com/galjos/odh-cli/internal/version.Commit=88f54601b9ec
+      -X github.com/galjos/odh-cli/internal/version.Date=2026-08-04T08:21:02Z
     ]
 
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/odh"
